f75758ce77292620084e94efee0218f0ea93a4b5
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-05-08 18:08:47 +0200

    operator for dividend and divider divider

--- StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
+++ StudentNo_Classcode_Group_Name-Surname_DJS02-main/scripts.js
@@ -6,4 +6,9 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = dividend / divider;
+
+  if (!dividend || !divider) {
+    result.classList.add("error-message")
+    result.innerText = "Division not performed. Both values are required in inputs. Try again.";
+    return;
 });
